65dabab69bbcadbeb64b350da794a2e7bddd3773
David Mateus <damateus964@misena.edu.co>
2019-07-01 11:40:19 -0500

    Listado vehiculos

---
+++ OMB_V2/Forms/Listados_vehiculos.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace OMB_V2.Forms
+{
+    partial class Listados_vehiculos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Listados_vehiculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 642);
+            this.Name = "Listados_vehiculos";
+            this.Text = "Listados_vehiculos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ OMB_V2/Forms/Listados_vehiculos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OMB_V2.Forms
+{
+    public partial class Listados_vehiculos : Form
+    {
+        public Listados_vehiculos()
+        {
+            InitializeComponent();
+        }
+    }
+}
