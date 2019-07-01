8ffa10f266cddc728745b9306958fa24c8077d76
David Mateus <damateus964@misena.edu.co>
2019-07-01 12:14:02 -0500

    Diseños al 50%

---
+++ OMB_V2/Forms/Listado_vigencias_polizas.Designer.cs
@@ -0,0 +1,39 @@
+﻿namespace OMB_V2.Forms
+{
+    partial class Listado_vigencias_polizas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Listado_vigencias_polizas";
+        }
+
+        #endregion
+    }
+}
