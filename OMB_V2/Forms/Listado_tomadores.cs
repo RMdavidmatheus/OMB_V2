67c4cd2508318d8514a28cb2282d8e10052b84fc
David Mateus <damateus964@misena.edu.co>
2019-08-04 10:20:20 -0500

    Listado de vigencias

--- OMB_V2/Forms/Listado_tomadores.cs
+++ OMB_V2/Forms/Listado_tomadores.cs
@@ -19,11 +19,13 @@ namespace OMB_V2.Forms
         // INICIALIZACION METODOS
         #region
         Capa_logica.Metodos Metodos = new Capa_logica.Metodos();
+        Capa_login.Metodos_login Metodos_mongo = new Capa_login.Metodos_login();
         #endregion
         // FIN
         private void Listado_tomadores_Load(object sender, EventArgs e)
         {
             // LLENAR EL DATAGRID CON EL METODO CORRESPONDIENTE PARA TENER EL DATA SOURCE
+            //Metodos_mongo.Listar_usuarios(Dtg_Listado_tomadores);
         }
     }
 }
